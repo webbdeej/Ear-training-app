6d0eac76037ecb3fb8ce3eba49ce23f675e41abb
Dave Webb <dw2.spiderfex@gmail.com>
2020-07-29 11:40:32 +0100

    Added audio triggers

--- JS/Projects/Ear-training-app/ear.js
+++ JS/Projects/Ear-training-app/ear.js
@@ -1,16 +1,38 @@
+//generate random number between 1 and 3
+
 document.getElementById("start-button").addEventListener("click", function () {
   let random = Math.floor(Math.random() * 3) + 1;
   console.log(random);
   setButtons(random);
+  triggerSounds(random);
 });
+
+//
+
+
+
+
+//write function to trigger sounds
+
+function triggerSounds(interval) {
+if (interval === 1) {
+  document.getElementById('maj-3-sound').play();
+} else if (interval === 2) {
+    document.getElementById('perf-5th-sound').play();
+} else (interval === 3) {
+  document.getElementById('octave-sound').play();
+ }
+
+}
+
 
 function setButtons(number) {
   let buttons = document.getElementsByTagName("button");
   for (let i = 0; i < buttons.length; i++) {
     buttons[i].onclick = function (e) {
-      if (number === 1 && this.id === "maj-2") {
-        alert("that's right - it was a major 2nd interval!");
-      } else if (this.id !== "maj-2" && this.id !== "start-button") {
+      if (number === 1 && this.id === "maj-3") {
+        alert("that's right - it was a major 3rd interval!");
+      } else if (this.id !== "maj-3" && this.id !== "start-button") {
         alert("You're wrong!");
       }
     };
